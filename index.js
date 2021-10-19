b7cb80c5fa06b074595c533fdc07663839d6e9b8
Mr. Richmond Gyekye <mrgyekye740@gmail.com>
2021-10-19 23:30:05 +0000

    adding styles

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-let name = "john";
-const myEmojis = [];
+const myEmojis = ["👨‍💻", "⛷", "🍲"];
+let myEmojiContainer = document.getElementById("emoji-container");
+
+
+/// looping through myEmoji Array
+//let emojiCounter = "";
+for (let i = 0; i < myEmojis.length; i++) {
+    let emojiSpan = document.createElement("span");
+    emojiSpan.textContent += myEmojis[i];
+    myEmojiContainer.append(emojiSpan);
+
+}
