991353ab150076401317940adc4bcd46edd4378e
Mr. Richmond Gyekye <mrgyekye740@gmail.com>
2021-10-20 00:56:06 +0000

    app completed complete: app

--- index.js
+++ index.js
@@ -1,12 +1,56 @@
 const myEmojis = ["👨‍💻", "⛷", "🍲"];
-let myEmojiContainer = document.getElementById("emoji-container");
+let myEmojiContainer = document.querySelector("#emoji-container");
+let pushBtn = document.querySelector("#push-btn");
+let unshiftBtn = document.querySelector("#unshift-btn");
+let emojiInput = document.querySelector("#emoji-input");
+let popBtn = document.querySelector("#pop-btn");
+let shiftBtn = document.querySelector("#shift-btn");
+
+renderArray(myEmojis);
+let newEmojiArray = [];
+
+pushBtn.addEventListener("click", function() {
+    if (emojiInput.value) {
+        myEmojis.push(emojiInput.value);
+        emojiInput.value = null;
+        renderArray(myEmojis);
+
+    } else {
+        alert("Input field is not supposd to be empty");
+    }
+});
+
+unshiftBtn.addEventListener("click", function() {
+    if (emojiInput.value) {
+        myEmojis.unshift(emojiInput.value);
+        emojiInput.value = null;
+
+        renderArray(myEmojis);
+
+    } else {
+        alert("Input field is not supposd to be empty");
+    }
+});
+
+popBtn.addEventListener("click", function() {
+    myEmojis.pop();
+    renderArray(myEmojis);
+
+});
+shiftBtn.addEventListener("click", function() {
+    myEmojis.shift();
+    renderArray(myEmojis);
+
+});
+
+function renderArray(arr) {
+    myEmojiContainer.innerHTML = null;
+    for (let i = 0; i < arr.length; i++) {
+        let emojiSpan = document.createElement("span");
+        emojiSpan.textContent += arr[i];
+        myEmojiContainer.append(emojiSpan);
 
 
-/// looping through myEmoji Array
-//let emojiCounter = "";
-for (let i = 0; i < myEmojis.length; i++) {
-    let emojiSpan = document.createElement("span");
-    emojiSpan.textContent += myEmojis[i];
-    myEmojiContainer.append(emojiSpan);
+    }
 
 }
